06f48520246bc4e3333a5292f4992727d479d78c
Prathipan <prathipan24p@gmail.com>
2022-09-23 22:02:21 +0530

    formik

--- src/components/header/CreateTask.js
+++ src/components/header/CreateTask.js
@@ -1,36 +1,28 @@
-import React,{useState} from "react";
+import { useFormik } from "formik";
+import React from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import * as yup from 'yup'
 
 const CreateTask = ({save ,show, setShow }) => {
-  const [TaskDetail, setTaskDetail] = useState({
-    task: "",
-    description: "",
-  });
+ 
+  const todoValidationSchema = yup.object({
+    task : yup.string().required("*Task is required"),
+    description : yup.string().required("Description is mandatory")
+  })
 
-  const handleChange = (event) => {
-
-    const { name, value } = event.target;
-
-        setTaskDetail((prev) => {
-            return {
-              ...prev,
-              [name]: value,
-            };
-          });
-    }
-
-    
-
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    save(TaskDetail);
-    setTaskDetail({
+  const formik = useFormik({
+    initialValues: {
       task : "",
       description : ""
-    })
-  }
+    },
+    validationSchema : todoValidationSchema,
+    onSubmit : (task , {resetForm}) => {
+      save(task);
+      resetForm({values:""});
+    }
+  })
 
   const handleClose = () => setShow(false);
 
@@ -39,25 +31,31 @@ const CreateTask = ({save ,show, setShow }) => {
       <Modal.Header closeButton>
         <Modal.Title>Create Task</Modal.Title>
       </Modal.Header>
-      <form className="m-3" onSubmit={handleSubmit}>
+      <form className="m-3" onSubmit={formik.handleSubmit}>
         <Form.Group className="m-2">
-          <Form.Control
+          <input
+            className="form-control"
             type="text"
+            id="task"
             name="task"
-            onChange={handleChange}
-            value={TaskDetail.task}
+            onChange={formik.handleChange}
+            value={formik.values.task}
             placeholder="Enter Task"
           />
+          <span style={{"color" : "red"}}>{formik.touched.task && formik.errors.task ? formik.errors.task : ""}</span>
         </Form.Group>
         <Form.Group className="m-2">
           <textarea
             className="form-control"
-            rows={5}
+            id="description"
             name="description"
-            onChange={handleChange}
-            value={TaskDetail.description}
+            rows={5}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.description}
             placeholder="Enter Description"
           />
+          <span style={{"color" : "red"}}>{formik.touched.description && formik.errors.description ? formik.errors.description : ""}</span>
         </Form.Group>
         <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
